9a0f6405c04dfc184f2b6fbb4e5ce8ed10544a6a
Édes Balázs <edesbalazs@gmail.com>
2015-09-20 23:30:33 +0200

    Removed unnecessary gulp dependency, switched to newest djs, added configurable djs version

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 var dogescript = require('dogescript'),
-	gulp = require('gulp'),
 	through2 = require('through2'),
 	gutil = require('gulp-util'),
 	PluginError = gutil.PluginError;
@@ -7,9 +6,14 @@ var dogescript = require('dogescript'),
 var PLUGIN_NAME = 'gulp-dogescript';
 
 function gulpDjsCompile(config) {
-	var beautify = config.hasOwnProperty('beautify') ? !!(config.beautify) : false;
-	var trueDoge = config.hasOwnProperty('trueDoge') ? !!(config.trueDoge) : false; // TODO warning
-  
+	var getOrDefault = function getOrDefault(object, property, defaultValue) {
+		return object.hasOwnProperty(property) ? object[property] : defaultValue;
+	};
+
+	var djs = getOrDefault(config, 'dogescript', dogescript);
+	var beautify = !!getOrDefault(config, 'beautify', false);
+	var trueDoge = !!getOrDefault(config, 'trueDoge', false); // TODO warning
+
 	return through2.obj(function (file, enc, cb) {
     if (file.isStream()) {
       this.emit('error', new PluginError(PLUGIN_NAME, 'Streams are not supported!'));
@@ -18,7 +22,7 @@ function gulpDjsCompile(config) {
 
     if (file.isBuffer()) {
 			var output = file.clone();
-			output.contents = new Buffer(dogescript(file.contents.toString(), beautify, trueDoge));
+			output.contents = new Buffer(djs(file.contents.toString(), beautify, trueDoge));
 			output.path = file.path.replace('.djs', '.js'); // TODO more sophisticated way to replace djs with js
 
 			this.push(output);
